3d07225455af5ef37d3816a65204721de2b10af8
Dave <davidnjau21@gmail.com>
2022-06-27 08:55:27 +0300

    feat(core): Solved bug

---
+++ County/src/main/java/com/dave/county/Utils.java
@@ -0,0 +1,25 @@
+package com.dave.county;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+    static String getJsonFromAssets(Context context) {
+        String jsonString;
+        String fileName = "county.json";
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonString = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return jsonString;
+    }
+}
